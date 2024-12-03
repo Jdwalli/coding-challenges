1d0cf7275151e494912b8455f41beb3dec55404c
Joshua Wallington <Jdwalli2003@yahoo.com>
2024-12-03 01:14:12 -0500

    Solved problem 2

--- advent_of_code/2024/day_2/solution.py
+++ advent_of_code/2024/day_2/solution.py
@@ -1,39 +1,35 @@
-from collections import Counter
+
+def is_safe(xs) -> bool:
+    increase_or_decrease = xs in [sorted(xs), sorted(xs, reverse=True)]
+    is_valid = True
+    for i in range(len(xs) - 1):
+        diff = abs(xs[i] - xs[i + 1])
+        if not 1 <= diff <= 3:
+            is_valid = False
+    return is_valid and increase_or_decrease
 
 
 def count_safe_reports(lst: list[str]) -> int:
-    safe_reports = 0
+    safe = 0
     for report in lst:
-        report = report.strip().split(" ")
-        level = []
-        is_valid = True
-        for i in range(len(report) - 1):
-            l, r = int(report[i]), int(report[i + 1])
-            # Any two adjacent levels differ by at least one and at most three.
-            if abs(l - r) > 3 or abs(l - r) < 1:
-                is_valid = False
-            if l > r:
-                level.append("+")
-            if l < r:
-                level.append("-")
-        
-        if is_valid and (all(x == "+" for x in level) or all(x == "-" for x in level)):
-            safe_reports += 1
-        
-    return safe_reports
+        report = list(map(int, report.split()))
+        if is_safe(report) == True:
+            safe += 1
+
+    return safe
 
 
-
-        
-        
-       
-    
-        
-    return safe_reports
-        
-
+def problem_dampener(lst: list[str]) -> int:
+    safe = 0
+    for report in lst:
+        reports = list(map(int, report.split()))
+        if (any(is_safe(reports[:index] + reports[index + 1:]) for index in range(len(reports)))):
+            safe += 1
+    return safe
 
 
 if __name__ == "__main__":
     with open('input.txt', 'r') as file:
         print(count_safe_reports(file.readlines()))
+        file.seek(0)
+        print(problem_dampener(file.readlines()))
